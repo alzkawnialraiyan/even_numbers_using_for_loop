6ef2a8a77a7b7a34b44a16f7dc4997c74f77a951
al raiyan al zakwani <alzakwanialraiyan0@gmail.com>
2023-08-18 12:56:36 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //even numbers using for loop
+        for(int even = 0; even <= 100; even++){
+            if( even%2 == 0)
+                System.out.println(even);
+        }
+    }
+}
